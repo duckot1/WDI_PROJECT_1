b68d62dffc0804310bde6e0396f96c4c82a89947
duckot1 <tom.duckworth91@gmail.com>
2016-12-10 17:34:27 +0000

    1 level of targets done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,52 @@
 var Game = Game || {};
 
+Game.hitListener2 = function(a, b) {
+  Game.targets = document.getElementsByClassName('target');
+  Game.targets[0].addEventListener('click', function(){
+    this.className = 'boxes';
+  });
+  Game.targets[1].addEventListener('click', function(){
+    this.className = 'boxes';
+  });
+  if (Game.targets.length > 0) {
+    Game.setTargetTwo(a + 10, b + 15);
+  }
+};
+
+Game.setTargetTwo = function(a, b) {
+  Game.boxes = document.getElementsByClassName('boxes');
+  Game.boxes[a].setAttribute('class', 'target');
+  Game.boxes[b].setAttribute('class', 'target');
+  Game.hitListener2(a, b);
+};
+
+Game.hitListener = function(b) {
+  Game.target = document.getElementsByClassName('target');
+  Game.target[0].addEventListener('click', function(){
+    Game.target[0].className = 'boxes';
+    if (b < 48){
+      Game.setTarget(b + 18);
+      console.log(b);
+    } else {
+      Game.setTargetTwo(14, 58);
+    }
+  });
+};
+
+
+Game.setTarget = function (a) {
+  Game.boxes = document.getElementsByClassName('boxes');
+  Game.boxes[a].setAttribute('class', 'target');
+  Game.hitListener(a);
+};
+
 Game.buildGrid = function () {
-  for (var i = 0; i < 144; i++) {
+  for (var i = 0; i < 80; i++) {
     Game.box = document.createElement('li');
     document.getElementById('grid').appendChild(Game.box);
+    Game.box.setAttribute('class', 'boxes');
   }
-  console.log(Game.box);
+  Game.setTarget(13);
 };
 
 
