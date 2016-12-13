03c9707c35570ef9b60969415e438f0953cb2f7f
duckot1 <tom.duckworth91@gmail.com>
2016-12-13 11:47:40 +0000

    changed to asteroids floating health added

--- js/app.js
+++ js/app.js
@@ -3,20 +3,40 @@ var Game = Game || {};
 Game.gameOver = function() {
   $('#range').append('<div class="finalScore"></div>');
   $('.finalScore').html('You scored ' + Game.$score.html() + '. Play Again?');
-  if (Game.$score > 100) {
+  if (parseInt((Game.$score).html()) > 100) {
     $('.finalScore').append('<button id="nextLevel">Next level</button>');
-    
+    $('#nextLevel').on('click', function(){
+      $('.finalScore').remove();
+      Game.enemyInterval = Game.enemyInterval * 0.8;
+      Game.moveAwaySpeed = Game.moveAwaySpeed * 0.8;
+      Game.timesRunOne = 0;
+      Game.timesRunTwo = 0;
+      Game.timesRunThree = 0;
+      Game.enemyOnetimes = Game.enemyOnetimes * 1.5;
+      Game.enemyTwotimes = Game.enemyTwotimes * 1.5;
+      Game.levels();
+    });
   } else {
     $('.finalScore').append('<button id="reset">Play Again</button>');
     $('#reset').on('click', function(){
       $('.finalScore').remove();
       Game.$score.html(0);
-      Game.levels.bind(Game);
+      Game.levels();
     });
   }
 
   console.log('gameOver');
 };
+
+Game.damage = function () {
+  if (document.getElementsByClassName('health').length === 1) {
+    $('#healthBox :last-child').remove();
+    console.log('Game over');
+  } else {
+    $('#healthBox :last-child').remove();
+  }
+};
+
 
 Game.addAmmo = function addAmmo() {
   var j = 0;
@@ -32,20 +52,36 @@ Game.reload = function reload() {
   this.reloadBox.addEventListener('click', this.addAmmo);
 };
 
-Game.moveAway = function(a, b) {
-  $(a).animate({left: b, top: '50px', height: '-=200px', width: '-=200px'}, 3000, function(){
+Game.moveAway = function(a, b, c) {
+  $(a).animate({left: b, top: c, height: '+=200px', width: '+=200px'}, Game.moveAwaySpeed, function(){
     $(this).remove();
+    Game.damage();
   });
 };
 
 Game.moveAcross = function(a, b) {
-  $(a).animate({left: b}, 3000, function(){
+  $(a).animate({left: b}, Game.moveAcrossSpeed, function(){
     $(this).remove();
   });
 };
 
+Game.healthRegen = function healthRegen() {
+  if (Game.ammo.length === 0) return console.log('reload');
+  $(this).stop();
+  $(this).remove();
+  if (document.getElementsByClassName('health').length < 5) {
+    $('#healthBox').append('<li class="health"></li>');
+  }
+  console.log('hit!');
+};
+
+Game.healthListener = function(a) {
+  $(a).on('click', this.healthRegen);
+};
+
 Game.enemyHit = function enemyHit() {
   if (Game.ammo.length === 0) return console.log('reload');
+  $(this).stop();
   $(this).remove();
   Game.$score.html(parseInt(Game.$score.html()) + 10);
   console.log('hit!');
@@ -56,23 +92,23 @@ Game.hitListener = function(a) {
 };
 
 Game.enemySelectorOne = function()  {
-  if (Game.timesRunOne < 15){
+  if (Game.timesRunOne < Game.enemyOnetimes){
     Game.timesRunOne += 1;
     var newEnemy = Math.floor(Math.random()*3);
     switch (newEnemy) {
       case 0:
         this.$range.append('<div id="enemyOne"></div>');
-        this.moveAway('#enemyOne', '0px');
+        this.moveAway('#enemyOne', '325px', '200px');
         this.hitListener('#enemyOne');
         break;
       case 1:
         this.$range.append('<div id="enemyTwo"></div>');
-        this.moveAway('#enemyTwo', '100%');
+        this.moveAway('#enemyTwo', '525px', '200px');
         this.hitListener('#enemyTwo');
         break;
       case 2:
         this.$range.append('<div id="enemyThree"></div>');
-        this.moveAway('#enemyThree', '0px');
+        this.moveAway('#enemyThree', '725px', '200px');
         this.hitListener('#enemyThree');
         break;
     }
@@ -82,23 +118,23 @@ Game.enemySelectorOne = function()  {
 };
 
 Game.enemySelectorTwo = function()  {
-  if (Game.timesRunTwo < 11){
+  if (Game.timesRunTwo < Game.enemyTwotimes){
     Game.timesRunTwo += 1;
     var newEnemy = Math.floor(Math.random()*3);
     switch (newEnemy) {
       case 0:
-        this.$range.append('<div id="enemyFour"></div>');
-        this.moveAway('#enemyFour', '100%');
+        this.$range.append('<div id="enemyFour">a</div>');
+        this.moveAway('#enemyFour', '325px', '400px');
         this.hitListener('#enemyFour');
         break;
       case 1:
-        this.$range.append('<div id="enemyFive"></div>');
-        this.moveAway('#enemyFive', '0px');
+        this.$range.append('<div id="enemyFive">a</div>');
+        this.moveAway('#enemyFive', '525px', '400px');
         this.hitListener('#enemyFive');
         break;
       case 2:
-        this.$range.append('<div id="enemySix"></div>');
-        this.moveAway('#enemySix', '100%');
+        this.$range.append('<div id="enemySix">a</div>');
+        this.moveAway('#enemySix', '725px', '400px');
         this.hitListener('#enemySix');
         break;
     }
@@ -107,25 +143,25 @@ Game.enemySelectorTwo = function()  {
   }
 };
 
-Game.enemySelectorThree = function()  {
+Game.healthSelector = function()  {
   if (Game.timesRunThree < 8){
     Game.timesRunThree += 1;
     var newEnemy = Math.floor(Math.random()*3);
     switch (newEnemy) {
       case 0:
-        this.$range.append('<div id="enemySeven"></div>');
-        this.moveAcross('#enemySeven', '100%');
-        this.hitListener('#enemySeven');
+        this.$range.append('<div id="healthOne"></div>');
+        this.moveAcross('#healthOne', '100%');
+        this.healthListener('#healthOne');
         break;
       case 1:
-        this.$range.append('<div id="enemyEight"></div>');
-        this.moveAcross('#enemyEight', '0px');
-        this.hitListener('#enemyEight');
+        this.$range.append('<div id="healthTwo"></div>');
+        this.moveAcross('#healthTwo', '0px');
+        this.healthListener('#healthTwo');
         break;
       case 2:
-        this.$range.append('<div id="enemyNine"></div>');
-        this.moveAcross('#enemyNine', '100%');
-        this.hitListener('#enemyNine');
+        this.$range.append('<div id="healthThree"></div>');
+        this.moveAcross('#healthThree', '100%');
+        this.healthListener('#healthThree');
         break;
     }
   } else {
@@ -150,18 +186,19 @@ Game.createEnemies = function () {
 
 Game.createEnemiesTwo = function () {
   Game.timesRunTwo = 0;
-  Game.spawn = setInterval(Game.enemySelectorTwo.bind(Game), 4000);
+  Game.spawn = setInterval(Game.enemySelectorTwo.bind(Game), Game.enemyInterval);
 };
 
-Game.createEnemiesThree = function () {
+Game.createHealth = function () {
   Game.timesRunThree = 0;
-  Game.spawn = setInterval(Game.enemySelectorThree.bind(Game), 4000);
+  Game.spawn = setInterval(Game.healthSelector.bind(Game), 10000);
 };
 
 Game.levels = function () {
   this.createEnemies();
+  this.createHealth();
   setTimeout(this.createEnemiesTwo, this.levelOneDelay);
-  setTimeout(this.createEnemiesThree, 30000);
+  setTimeout(this.gameOver, this.gameLength);
 };
 
 // Game.controls = function() {
@@ -188,14 +225,17 @@ Game.buildGrid = function () {
   this.reloadBox        = document.getElementById('reload');
   this.$range           = $('#range');
   this.levelOneDelay    = 18000;
-  this.gameLength       = 5000;
+  this.gameLength       = 70000;
   this.$score           = $('#score');
+  Game.moveAwaySpeed    = 3000;
+  Game.moveAcrossSpeed  = 3000;
+  Game.enemyOnetimes    = 16;
+  Game.enemyTwotimes    = 11;
 
   this.addAmmo();
   this.shotListener();
   this.reload();
   this.levels();
-  setTimeout(this.gameOver, this.gameLength);
 };
 
 // Game.startMenu = function() {
